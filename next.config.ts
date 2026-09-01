771ec421dc11ab2e0732678771e79419e39700e3
Shakib Khan <ichshakib@gmail.com>
2026-09-01 22:40:16 +0600

    feat: expose necessary environment variables in next.config.ts for service integration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_ENDPOINT: process.env.AWS_ENDPOINT,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
+  },
 }
 
 export default nextConfig
